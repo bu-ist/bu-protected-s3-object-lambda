77c19e67930af146b7cf957974ba4d4afd04969d
Jonathan Williams <tamlin@mac.com>
2023-01-03 18:10:18 -0500

    return unprocessed file

--- src/app.js
+++ src/app.js
@@ -4,18 +4,18 @@
 
 const { S3 } = require("aws-sdk");
 const axios = require("axios").default;  // Promise-based HTTP requests
-const sharp = require("sharp"); // Used for image resizing
+//const sharp = require("sharp"); // Used for image resizing
 
 const s3 = new S3();
 
 exports.handler = async (event) => {
   // Output the event details to CloudWatch Logs.
-  console.log("Event:\n", JSON.stringify(event, null, 2));
+  //console.log("Event:\n", JSON.stringify(event, null, 2));
 
   // Retrieve the operation context object from the event.
   // This contains the info for the WriteGetObjectResponse request.
   // Includes a presigned URL in `inputS3Url` to download the requested object.
-  const { getObjectContext } = event;
+  const { userRequest, getObjectContext } = event;
   const { outputRoute, outputToken, inputS3Url } = getObjectContext;
 
   // Get image stored in S3 accessible via the presigned URL `inputS3Url`.
@@ -24,13 +24,13 @@ exports.handler = async (event) => {
   // Resize the image
   // Height is optional, will automatically maintain aspect ratio.
   // withMetadata retains the EXIF data which preserves the orientation of the image.
-  const resized = await sharp(data).resize({ width: 100, height: 100 }).withMetadata();
+  //const resized = await sharp(data).resize({ width: 100, height: 100 }).withMetadata();
 
   // Send the resized image back to S3 Object Lambda.
   const params = {
     RequestRoute: outputRoute,
     RequestToken: outputToken,
-    Body: resized,
+    Body: data,
     ContentType: headers["content-type"],
   };
   await s3.writeGetObjectResponse(params).promise();
