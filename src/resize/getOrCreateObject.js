a2b822b67e849bc91983c982a13768dc611d2800
Jonathan Williams <tamlin@mac.com>
2023-02-27 12:55:00 -0500

    remove logging

--- src/resize/getOrCreateObject.js
+++ src/resize/getOrCreateObject.js
@@ -26,21 +26,18 @@ async function getOrCreateObject(url) {
   // The s3 key is the pathname without the leading slash.
   const s3Key = pathname.replace(/^\//, '');
 
-  console.log('Getting object', s3Key);
-
+  // Try to get the object from S3.
   const response = await tryGetObject(s3Key);
 
   // if the image is not found, and there is a size match, then resize the image and save it to S3.
   const sizeMatch = s3Key.match(/-(\d+)x(\d+)\.(jpg|png)$/);
   if (response.code === 'NoSuchKey' && sizeMatch) {
-    console.log('Resizing image');
     // Reconstruct what the original image s3 key would be, by removing the image size from the URL.
     const originalKey = s3Key.replace(/-(\d+)x(\d+)\.(jpg|png)$/, '.$3');
 
     const originalResponse = await tryGetObject(originalKey);
     // If there's no original image, then return the 404 response.
     if (originalResponse.code === 'NoSuchKey') {
-      console.log('Original image not found');
       return response;
     }
     // If there is an original, resize the image data with sharp and save it for future requests.
