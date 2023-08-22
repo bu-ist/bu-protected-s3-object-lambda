b69a50ceb7e6f6f31202848e953bdf93853af239
Jonathan Williams <tamlin@mac.com>
2023-08-22 09:58:31 -0400

    add unit test for protected sites

--- src/app.test.js
+++ src/app.test.js
@@ -1,8 +1,26 @@
 const { mockClient } = require('aws-sdk-client-mock');
 const { GetObjectCommand, S3Client } = require('@aws-sdk/client-s3');
 
+const { DynamoDBDocumentClient, GetCommand } = require('@aws-sdk/lib-dynamodb');
+
 // Require the function to test.
 const { handler } = require('./app');
+
+const ddbMock = mockClient(DynamoDBDocumentClient);
+
+process.env.DYNAMODB_TABLE = 'test-table';
+
+// Mock the ddb client.
+ddbMock.on(GetCommand, {
+  Key: { SiteAndGroupKey: 'PROTECTED_SITES' },
+}).resolves({
+  Item: {
+    ProtectedSites: JSON.stringify([
+      { 'protectedsite.host.bu.edu': 'somegroup' },
+      { 'example.host.bu.edu': 'anothergroup' },
+    ]),
+  },
+});
 
 // Mock the s3 client.
 const s3Mock = mockClient(S3Client);
