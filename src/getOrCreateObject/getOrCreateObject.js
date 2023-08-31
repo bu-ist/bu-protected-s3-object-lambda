a0b51e86942f1fdf26262378c7bd5008dbd3e697
Alex Caloggero <acalog@bu.edu>
2023-08-31 09:33:04 -0400

    Pad s3Key before checking it exists as to guarantee failure

--- src/getOrCreateObject/getOrCreateObject.js
+++ src/getOrCreateObject/getOrCreateObject.js
@@ -60,7 +60,8 @@ async function getOrCreateObject(url, domain) {
 
   // Remove double slashes from the s3 key, in the case of a missing domain.... this shouldn't be necessary there has to be a better way.
   s3Key = s3Key.replace(/\/\//g, '/');
-
+  //@acalog edit
+  s3Key = s3Key.padEnd(domain.length, '+_+');
   // Try to get the object from S3.
   const response = await tryGetObject(s3Key);
 
