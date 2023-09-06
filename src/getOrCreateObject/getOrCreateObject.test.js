5d757f3d24e933d3bab24c4023e3bc3a5fb485c2
Hennemuth <wrh@bu.edu>
2023-09-06 11:14:05 -0400

    Fixing tests

--- src/getOrCreateObject/getOrCreateObject.test.js
+++ src/getOrCreateObject/getOrCreateObject.test.js
@@ -25,7 +25,10 @@ s3Mock.on(GetObjectCommand).resolves({
 describe('getOrCreateObject', () => {
   it('should return an object', async () => {
     const result = await getOrCreateObject(
-      'https://example-1111.s3-object-lambda.us-east-1.amazonaws.com/somesite/files/01/example-150x150.jpg',
+      { 
+        url: 'https://example-1111.s3-object-lambda.us-east-1.amazonaws.com/somesite/files/01/example-150x150.jpg',
+        headers: { }
+      },
       'www.bu.edu',
     );
     expect(result.Body).toBeDefined();
@@ -33,7 +36,10 @@ describe('getOrCreateObject', () => {
 
   it('should return an an object, and parse the crop value', async () => {
     const result = await getOrCreateObject(
-      'https://example-1111.s3-object-lambda.us-east-1.amazonaws.com/files/01/example-758x460.jpg?resize-position=left',
+      { 
+        url: 'https://example-1111.s3-object-lambda.us-east-1.amazonaws.com/files/01/example-758x460.jpg?resize-position=left',
+        headers: { }
+      },
       'www.bu.edu',
     );
     expect(result.Body).toBeDefined();
